2ecc53977235f951f8ff83617d2f24b281c115c1
luca-de-bar <lucadebar33@gmail.com>
2024-08-28 18:02:25 +0200

    add : added show

--- spring-jpa-pizzeria/src/main/java/com/java_lessons/spring_jpa_pizzeria/controllers/IndexController.java
+++ spring-jpa-pizzeria/src/main/java/com/java_lessons/spring_jpa_pizzeria/controllers/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
@@ -16,14 +17,23 @@ public class IndexController {
     @Autowired
     private PizzaRepository pizzaRepository;
 
-    //HOME
-    @GetMapping("/")
+    //INDEX
+    @GetMapping
     public String index (Model model){
         //Prendo i dati da consegnare al modello
         List<Pizza> pizzas = pizzaRepository.findAll();
 
         //Inserisco dati nel modello
-        model.addAttribute("pizza",pizzas);
-        return "home";
+        model.addAttribute("pizzas",pizzas);
+        return "index";
+    }
+
+    //SHOW
+    @GetMapping("/ordina/{id}")
+    public String ordina(@PathVariable("id") Long id, Model model){
+
+        model.addAttribute("pizza",pizzaRepository.findById(id).get());
+
+        return "ordina";
     }
 }
