e20445f88b4be69730561c23c3cdcec233750b6c
luca-de-bar <lucadebar33@gmail.com>
2024-09-29 10:37:21 +0200

    ref: changed from Discount.java to Offer.java

---
+++ spring-jpa-pizzeria/src/main/java/com/java_lessons/spring_jpa_pizzeria/models/Offer.java
@@ -0,0 +1,57 @@
+package com.java_lessons.spring_jpa_pizzeria.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Discount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "pizza_id", nullable = false)
+    private Pizza pizza;
+
+    public Discount(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void setPizza(Pizza pizza) {
+        this.pizza = pizza;
+    }
+}
